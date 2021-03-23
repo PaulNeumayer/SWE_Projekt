edabad8c92db2451803778b1d0eb859933f6b5e6
Lukas Harrer <lukas.harrer@studentsfhwelsat.onmicrosoft.com>
2021-03-23 13:33:45 +0100

    change GUI

--- SWE_Projekt_Neumayer/SWE_Projekt_Neumayer/AddFom.Designer.cs
+++ SWE_Projekt_Neumayer/SWE_Projekt_Neumayer/AddFom.Designer.cs
@@ -48,99 +48,111 @@ namespace SWE_Projekt_Neumayer
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(97, 244);
+            this.label4.Location = new System.Drawing.Point(129, 300);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.Size = new System.Drawing.Size(103, 17);
             this.label4.TabIndex = 23;
             this.label4.Text = "date of change";
             // 
             // textBox5
             // 
             this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(198, 241);
+            this.textBox5.Location = new System.Drawing.Point(264, 297);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
             this.textBox5.TabIndex = 22;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(124, 157);
+            this.label3.Location = new System.Drawing.Point(165, 193);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.Size = new System.Drawing.Size(69, 17);
             this.label3.TabIndex = 21;
             this.label3.Text = "last name";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(124, 115);
+            this.label1.Location = new System.Drawing.Point(165, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.Size = new System.Drawing.Size(70, 17);
             this.label1.TabIndex = 20;
             this.label1.Text = "first name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(144, 73);
+            this.label2.Location = new System.Drawing.Point(192, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(42, 17);
             this.label2.TabIndex = 19;
             this.label2.Text = "Email";
             // 
             // label_Nr
             // 
             this.label_Nr.AutoSize = true;
-            this.label_Nr.Location = new System.Drawing.Point(158, 31);
+            this.label_Nr.Location = new System.Drawing.Point(211, 38);
+            this.label_Nr.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Nr.Name = "label_Nr";
-            this.label_Nr.Size = new System.Drawing.Size(18, 13);
+            this.label_Nr.Size = new System.Drawing.Size(23, 17);
             this.label_Nr.TabIndex = 18;
             this.label_Nr.Text = "Nr\r\n";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(198, 154);
+            this.textBox4.Location = new System.Drawing.Point(264, 190);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
             this.textBox4.TabIndex = 17;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(198, 112);
+            this.textBox3.Location = new System.Drawing.Point(264, 138);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
             this.textBox3.TabIndex = 16;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(198, 70);
+            this.textBox2.Location = new System.Drawing.Point(264, 86);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 15;
             // 
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(198, 28);
+            this.textBox1.Location = new System.Drawing.Point(360, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 14;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(347, 296);
+            this.button2.Location = new System.Drawing.Point(463, 364);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 13;
             this.button2.Text = "ok";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(14, 296);
+            this.button1.Location = new System.Drawing.Point(19, 364);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 12;
             this.button1.Text = "canel";
             this.button1.UseVisualStyleBackColor = true;
@@ -148,25 +160,27 @@ namespace SWE_Projekt_Neumayer
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(131, 202);
+            this.label5.Location = new System.Drawing.Point(175, 249);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.Size = new System.Drawing.Size(58, 17);
             this.label5.TabIndex = 25;
             this.label5.Text = "balance";
             // 
             // textBox6
             // 
             this.textBox6.Enabled = false;
-            this.textBox6.Location = new System.Drawing.Point(198, 199);
+            this.textBox6.Location = new System.Drawing.Point(264, 245);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.Size = new System.Drawing.Size(132, 22);
             this.textBox6.TabIndex = 24;
             // 
             // AddFom
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 331);
+            this.ClientSize = new System.Drawing.Size(579, 407);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label4);
@@ -181,6 +195,7 @@ namespace SWE_Projekt_Neumayer
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AddFom";
             this.Text = "Add";
             this.ResumeLayout(false);
